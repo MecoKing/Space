bba02e40b7c10d778b5c153b9ca1e1ec6fa8c520
Micah Merswolke <mecoking@icloud.com>
2014-02-19 17:24:46 -0500

    Added A SPACESystem class
    Will be used to generate and manage solar systems

---
+++ Space/SPACESystem.h
@@ -0,0 +1,15 @@
+//
+//  SPACESystem.h
+//  Space
+//
+//  Created by [pixelmonster] on 2/19/2014.
+//  Copyright (c) 2014 [pixelmonster]. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface SPACESystem : SKNode
+
+
+
+@end
---
+++ Space/SPACESystem.m
@@ -0,0 +1,17 @@
+//
+//  SPACESystem.m
+//  Space
+//
+//  Created by [pixelmonster] on 2/19/2014.
+//  Copyright (c) 2014 [pixelmonster]. All rights reserved.
+//
+
+#import "SPACESystem.h"
+
+@implementation SPACESystem
+
+//-(id)init {
+//    
+//}
+
+@end
