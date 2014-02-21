d4346b01aca28fd5e232125200717cd3663fb08f
Micah Merswolke <mecoking@icloud.com>
2014-02-21 12:14:39 -0500

    Added a class that will (in the future) hold all the functions

---
+++ Space/SPACEFunction.h
@@ -0,0 +1,14 @@
+//
+//  SPACEFunction.h
+//  Space
+//
+//  Created by [pixelmonster] on 2/21/2014.
+//  Copyright (c) 2014 [pixelmonster]. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface SPACEFunction : NSObject
+
+@end
---
+++ Space/SPACEFunction.m
@@ -0,0 +1,13 @@
+//
+//  SPACEFunction.m
+//  Space
+//
+//  Created by [pixelmonster] on 2/21/2014.
+//  Copyright (c) 2014 [pixelmonster]. All rights reserved.
+//
+
+#import "SPACEFunction.h"
+
+@implementation SPACEFunction
+
+@end
